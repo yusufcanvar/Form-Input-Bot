c36a3aad9836ac2f519c1f3c7467f9039bcf83bf
yusufcanvar <75281979+yusufcanvar@users.noreply.github.com>
2021-10-25 21:27:39 +0300

    2

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 
 function BotOpen() {
-    var mywindow = window.open("https://aosdestek.anadolu.edu.tr/soru-sor", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=900,left=500,width=800,height=1000");
+    // var mywindow = window.open("https://aosdestek.anadolu.edu.tr/soru-sor", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=900,left=500,width=800,height=1000");
+    /*
+        setTimeout(function(){
+            mywindow.document.getElementById('tc').value = '324234';
+        },2000)
+        */
 
+    //document.getElementById('tc').value = '324234'
+    document.getElementById('screen').contentWindow.document.getElementById('tc').value = 'x'
 
-    mywindow.onload = function () {
-        mywindow.document.getElementById('tc').value = '324234';
-    }
+    var iframe = document.getElementById("screen");
+    var elmnt = iframe.contentWindow.document.getElementsById('tc');
+    elmnt.style.display = "none";
 }
