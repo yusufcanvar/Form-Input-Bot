97e7b7b1958c96a6a1c2ecc85ad0257eb1401234
yusufcanvar <75281979+yusufcanvar@users.noreply.github.com>
2021-10-26 23:26:56 +0300

    x

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 
 function BotOpen() {
-    // var mywindow = window.open("https://aosdestek.anadolu.edu.tr/soru-sor", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=900,left=500,width=800,height=1000");
-    /*
+     var mywindow = window.open("https://aosdestek.anadolu.edu.tr/soru-sor", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=900,left=500,width=800,height=1000");
+ 
         setTimeout(function(){
             mywindow.document.getElementById('tc').value = '324234';
         },2000)
-        */
-
-    //document.getElementById('tc').value = '324234'
+      
     document.getElementById('screen').contentWindow.document.getElementById('firstName').value = 'Hüsnüüüü';
 /*
     var iframe = document.getElementById("screen");
